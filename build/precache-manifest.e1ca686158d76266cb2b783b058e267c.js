d9be598efd6ddd43bf7778f465e8371fde7ce5f6
Jeremy Black <jjb@Jeremys-MacBook-Pro.local>
2019-11-21 13:22:39 -0700

    inital commit

---
+++ build/precache-manifest.e1ca686158d76266cb2b783b058e267c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abdc73b1b5249d4d5154a60b6507e7b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "078ee22af5042ecf58f2",
+    "url": "/static/css/main.fe8eed3a.chunk.css"
+  },
+  {
+    "revision": "849d5b9389b6c699e293",
+    "url": "/static/js/2.92b37e90.chunk.js"
+  },
+  {
+    "revision": "078ee22af5042ecf58f2",
+    "url": "/static/js/main.7d6d1449.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
